0f1dad99394c6537d66b0ea84911c7f28d5e93d5
LibbyMills <millsjelizabeth@gmail.com>
2023-01-23 19:55:18 +0000

    Altered to match SheCodes solution

--- src/app.js
+++ src/app.js
@@ -1,25 +1,9 @@
 function findTime(event) {
-  let parisTime = moment()
-    .tz("Europe/Paris")
-    .format("dddd, MMMM, D, YYYY h:mm A");
-  let tokyoTime = moment()
-    .tz("Asia/Tokyo")
-    .format("dddd, MMMM, D, YYYY h:mm A");
-  let sydneyTime = moment()
-    .tz("Australia/Sydney")
-    .format("dddd, MMMM, D, YYYY h:mm A");
-
   if (event.target.value.length > 0) {
-    if (event.target.value === "paris") {
-      alert(`It is ${parisTime} in Europe/Paris`);
-    }
-    if (event.target.value === "tokyo") {
-      alert(`It is ${tokyoTime} in Europe/Paris`);
-    }
-
-    if (event.target.value === "sydney") {
-      alert(`It is ${sydneyTime} in Australia/Sydney`);
-    }
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM, D, YYYY h:mm A");
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 
